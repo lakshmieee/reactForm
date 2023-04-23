288f88224b1aa1449782703ebbf64dd774947357
lakshmieee <shivahari583@gmail.com>
2023-04-23 11:37:33 +0530

    demo change

--- src/app/srvc-interceptor.interceptor.ts
+++ src/app/srvc-interceptor.interceptor.ts
@@ -10,12 +10,12 @@ import {
 import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
-import { ToastrService } from 'ngx-toastr'; 
+// import { ToastrService } from 'ngx-toastr'; 
 
 @Injectable()
 export class SrvcInterceptorInterceptor implements HttpInterceptor {
 
-  constructor(private toastr: ToastrService) { } 
+  // constructor(private toastr: ToastrService) { } 
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     // let httphead= new HttpHeaders().set('Authorization', "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c");
